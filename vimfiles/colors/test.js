0cfd336a956c92dd5f3667dc552b50bc966f665e
Lynn <396976631@qq.com>
2012-08-10 19:09:33 +0800

    colors

---
+++ vimfiles/colors/test.js
@@ -0,0 +1,9 @@
+window.addEvent('domready', function(){
+   var name = function() {
+        var container = new Element('div', { id: 'side-container' });
+        container.adopt(document.getElements('.slide'))
+   }
+    if (a == 3) {
+        break;
+    }
+});
